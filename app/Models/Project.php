58c55cc0791efc144f79847442c3135dcfe0e77d
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-27 15:00:32 +0200

    Add Project_Technology Pivot Table + Edit Models Commit

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Technology;
 
 class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'slug', 'link', 'content', 'cover_image', 'video'];
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
